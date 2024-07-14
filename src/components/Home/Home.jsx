2f7133b2932e96629ad436df1c14f38063aaaf0c
towhid046 <towhidmorol46@gmail.com>
2024-07-15 01:32:03 +0600

    added loading spinner

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -53,12 +53,8 @@ const Home = () => {
 
   if (loading) {
     return (
-      <div className=" container">
-        <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
-        <div className="flex flex-col gap-4">
-          <div className="skeleton h-4 w-20"></div>
-          <div className="skeleton h-4 w-28"></div>
-        </div>
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading-spinner loading loading-lg text-[#8774E1]"></span>
       </div>
     );
   }
